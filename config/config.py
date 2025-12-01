ede535df7997f4076ce272fa1c3a10bca80ca8f3
blgperfect <135395604+blgperfect@users.noreply.github.com>
2025-12-01 16:33:04 -0500

    first commit

--- config/config.py
+++ config/config.py
@@ -1,17 +1,8 @@
 CONFIG = {
-    "welcome_channel": None,
-    "main_color": 0x5865F2,
 
     # ← ICI ton preset d'embed réuni sous UN SEUL MOT
     "embed_default": {
         "color": 0xEECBFF,
         "footer": "Nymbot @2025"
-    },
-
-    "systems": {
-        "welcome_enabled": True,
-        "goodbye_enabled": True,
-        "leveling_enabled": False,
-        "logging_enabled": True
     }
 }
